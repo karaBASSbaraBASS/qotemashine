c382ed5a09ffafd766de0e6c3e32cf8d1b597c41
Jack <evgen.mikhalov@gmail.com>
2019-05-11 03:16:02 +0300

    twitter post fix

--- quote_and_colorChange.js
+++ quote_and_colorChange.js
@@ -1,11 +1,16 @@
+// global variables
+var currentQuote = '';
+var currentAuthor = '';
+var qurl = '';
+var aurl = ''; 
+var curPost = '';
 
 function generate_quote (){
 var colors = ["#589e9b","#6de871","#adaa00","#e8ab3f","#ff7051"];
 //var colors = ["rgb(88, 158, 155)","rgb(109, 232, 113)","rgb(173, 170, 0)","rgb(232, 171, 63)","rgb(255, 112, 81)"];
 var rand = Math.floor(Math.random() * colors.length);
 var color_chose = colors[rand];
-var currentQuote = '';
-var currentAuthor = '';
+
  // go to to https://market.mashape.com/ 
  // log in using "X-Mashape-Key"
  // chose widget "random-famous-quotes"
@@ -15,29 +20,16 @@ var currentAuthor = '';
       "X-Mashape-Key": "f6BF6Cgiz3mshky0UAk3hLtznYP4p1Fn3UUjsnanA1TjSUPOTu",
       "X-RapidAPI-Host": "andruxnet-random-famous-quotes.p.rapidapi.com",
       "Content-Type": "application/x-www-form-urlencoded"
-    }, // header close
-    
-// if login sucsess - parse quote and autor name
+    },
     success: function(response){   
-      
       var resp = response[0];
       currentQuote = resp.quote;
-      currentAuthor = resp.author; 
-  
-//$('#new-quote').on('click', generate_quote ());
-// in this case gonna be link creating script    
-    var qurl = resp.quote.replace(/[ /]/g,'%20');
-    var aurl = resp.author.replace(/[ /]/g,'%20'); 
-  $('#tweet-quote').on('click', function() {
-    window.open('https://twitter.com/intent/tweet?hashtags=quotes&text="'+ qurl +'" -' + aurl); 
-  });
-      
- //--------don't figure out how to create "tumblr post"     
-  // $('#tumblr-quote').on('click', function() {
-  //   window.open("https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&title=Quote%20Machine&content=" + qurl + "&caption=" + aurl);
-  // });
-//---------don't figure out how to create "tumblr post" 
-      
+      currentAuthor = resp.author;
+      // replace all spaces to spaces-code symboll - "%20"
+      qurl = currentQuote.replace(/[ /]/g,'%20');
+      aurl = currentAuthor.replace(/[ /]/g,'%20');
+      // generate twetter post link
+      curPost = 'https://twitter.com/intent/tweet?hashtags=quotes&text="'+ qurl +'" -' + aurl;     
 // fade qote block in 500ms   
     $("#aphorism").animate({
           opacity: 0
@@ -50,7 +42,7 @@ var currentAuthor = '';
           $('#text').text(currentQuote);
         });
 // fade qote block in 500ms 
-      $("#aphorism-autor").animate({
+    $("#aphorism-autor").animate({
           opacity: 0
         }, 500,
  // past response to the qote block and appear it in 500ms
@@ -72,8 +64,13 @@ var currentAuthor = '';
         backgroundColor: color_chose
       }, 1000);
   
-      } //success close
+    } //success close
   }); // ajax close
 }; //generate quote close
+// open new window with tweet on click
+function openTweet() {
+    console.log(curPost)
+    window.open(curPost); 
+};
 
 
